c21b7749b88ae4bf1ac497dac0c85f5c6612a9bb
yersonargote <yersonargote@unicauca.edu.co>
2023-11-16 16:53:18 -0500

    Full tutorial Ollama-LangChain in local

---
+++ main.py
@@ -0,0 +1,78 @@
+from langchain import hub
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import BaseRetrievalQA
+from langchain.docstore.document import Document
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import GPT4AllEmbeddings  # , OllamaEmbeddings
+from langchain.llms import Ollama
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+QA_CHAIN_PROMPT = hub.pull("rlm/rag-prompt-llama")
+
+
+def load_file(path: str) -> list[Document]:
+    loader = TextLoader(path)
+    data: list[Document] = loader.load()
+    return data
+
+
+def split(data: list[Document]) -> list[Document]:
+    text_spliter = RecursiveCharacterTextSplitter(
+        chunk_size=1500,
+        chunk_overlap=100,
+    )
+    splits: list[Document] = text_spliter.split_documents(data)
+    return splits
+
+
+def embed(data: list[Document]):
+    vectorstore: Chroma = Chroma.from_documents(
+        documents=data,
+        embedding=GPT4AllEmbeddings(),
+    )
+    return vectorstore
+
+
+def retrieve(question: str, vectorstore: Chroma) -> str:
+    answer: str = vectorstore.similarity_search(question)
+    return answer
+
+
+def rag(question: str, vectorstore: Chroma):
+    llm = Ollama(
+        # base_url="http://localhost:11434",
+        model="llama2:13b",
+        verbose=True,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    )
+
+    qa_chain: BaseRetrievalQA = RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+    )
+    answer = qa_chain({"query": question})
+    return answer
+
+
+def main() -> None:
+    path: str = "./cuento.txt"
+
+    data: list[Document] = load_file(path)
+    data = split(data)
+
+    embeddings = embed(data)
+
+    question: str = "¿Qué deseaba hacer Salto que lo hacía diferente de los otros sapos?. Dame la respuesta en Español y de forma corta."
+
+    answer: str = retrieve(question, embeddings)
+
+    answer = rag(question, embeddings)["query"]
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Following tutorial for learn about how to use Ollama
+
+
+- [***Ollama lang-chaing tutorial***](https://python.langchain.com/docs/integrations/llms/ollama)
+
+- [***Github official Ollama repo***](https://github.com/jmorganca/ollama)
+
+- [***Ollama llama-index tutorial***](https://gpt-index.readthedocs.io/en/stable/examples/llm/ollama.html)
